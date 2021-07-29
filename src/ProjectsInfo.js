1b72c08209ad129277a31b2bcda35a0184c8dbdc
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-07-29 16:50:29 -0500

    Images added
    Also the links were modified in the ProjectsInfo object.

--- src/ProjectsInfo.js
+++ src/ProjectsInfo.js
@@ -1,7 +1,9 @@
 import image from "./images/Logo_AshGray.png";
+import capture from "./images/capture.png";
+import chillhop from "./images/chillhop.png";
 
 export const projectsInfo = [
   { name: "Bokushop", img: image },
-  { name: "Chillhop player", img: image },
-  { name: "Capture Photography", img: image },
+  { name: "Chillhop player", img: chillhop },
+  { name: "Capture Photography", img: capture },
 ];
