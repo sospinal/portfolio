e39780c7d62542fb75bc2721e444e2bdd62c45c2
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-08-04 14:26:04 -0500

    Styling modified
    -ContainerBlock component deleted because it was redundant with the
     Container styled div.
    -Container is modified to include the padding of ContainerBlock.
    -align-items added to Row.
    -A styled div called ImageContainer is added for putting images inside
     and setting their layout in a Row.

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,21 +1,21 @@
-import React from "react";
 import styled from "styled-components/macro";
-
-export const ContainerBlock = ({ children }) => {
-  return <Container>{children}</Container>;
-};
 
 export const Container = styled.div`
   margin: auto;
   display: flex;
-  padding: 3rem 6rem;
+  padding: 3rem 12rem;
   flex-direction: column;
   text-align: center;
   @media all and (max-width: 1200px) {
-    padding: 3rem 4rem;
+    padding: 3rem 8rem;
   }
   @media all and (max-width: 1008px) {
-    padding: 3rem 3rem;
+    padding: 3rem 6rem;
+    padding-bottom: 0rem;
+  }
+  @media all and (max-width: 768px) {
+    padding: 3rem 2rem;
+    padding-bottom: 0rem;
   }
 `;
 
@@ -24,9 +24,29 @@ export const Row = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
+  align-items: center;
   min-height: 20vh;
   @media all and (max-width: 768px) {
     flex-direction: column;
     align-items: center;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  width: 25%;
+  min-width: 150px;
+  @media all and (max-width: 768px) {
+    width: 60%;
+    padding: 2rem 0rem;
+  }
+  @media all and (max-width: 576px) {
+    width: 80%;
+  }
+  img {
+    border-radius: 10px;
+    height: auto;
+    width: 100%;
+    object-fit: cover;
   }
 `;
