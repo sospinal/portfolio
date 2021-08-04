6d28820e3a9805562c8d33c340cda941523e8124
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-08-04 14:11:47 -0500

    Projects.js filename becomes AllProjects.js

---
+++ src/components/AllProjects.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Row } from "../styles/styles";
+import { projectsInfo } from "../assets/ProjectsInfo";
+import Project from "./ProjectComponent";
+
+const Projects = () => {
+  return (
+    <Row>
+      {projectsInfo.map((project) => {
+        return <Project img={project.img} name={project.name} />;
+      })}
+    </Row>
+  );
+};
+
+export default Projects;
