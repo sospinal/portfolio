4806c21f665b665876f0db201b3136b706ad789d
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-08-04 14:05:29 -0500

    New projects pictures added and ProjectsInfo moved
    -ProjectsInfo.js is moved inside the assets folder.
    -The paths for the projects images are added to ProjectsInfo.

--- src/assets/ProjectsInfo.js
+++ src/assets/ProjectsInfo.js
@@ -1,6 +1,6 @@
-import image from "./images/Logo_AshGray.png";
-import capture from "./images/capture.png";
-import chillhop from "./images/chillhop.png";
+import image from "../images/Logo_AshGray.png";
+import capture from "../images/capture.png";
+import chillhop from "../images/chillhop.png";
 
 export const projectsInfo = [
   { name: "Bokushop", img: image },
